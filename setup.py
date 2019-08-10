9a4f23a20e72df37c799ca3fd7f55d01d6e7731f
Trevor Taubitz <terrabitz@protonmail.com>
2019-08-10 01:43:53 -0500

    ci(travis): Fix multiple file upload specification

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='1.0',
     description='Easy testing for Rubber Ducky Scripts',
     author='Trevor Taubitz',
-    author_email='tjtaubit@mtu.edu',
+    author_email='terrabitz@protonmail.com',
     packages=['QuackTest'],
     scripts=['bin/quacktest.py', 'bin/quacktest-gui.py'],
     url='https://github.com/terrabitz/QuackTest',
